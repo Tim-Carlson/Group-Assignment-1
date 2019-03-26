a377adc92b4a0690f432e594eea43faf9e310916
ld1397zm <46947914+ld1397zm@users.noreply.github.com>
2019-03-26 17:14:52 -0500

    JSONReader now working
    The JSON Reader now takes an ArrayList<Study> and populates it with entries matching data from the JSON. Tested and works with JSON produced by the JSONWriter.

--- contents/JsonReader.java
+++ contents/JsonReader.java
@@ -6,87 +6,116 @@ import org.json.simple.*;
 import org.json.simple.parser.*;
 
 public class JsonReader {
-
 	private JSONParser parser;
-	private Site sitePlaceholder;
 
 	public JsonReader() {
 		parser = new JSONParser();
-
 	}
 
 	@SuppressWarnings("unchecked")
 	public boolean read(String location, ArrayList<Study> studyPlaceholder) {
-		String siteId, readingValue, readingDate, readingId, readingType, studyName, studyID;
+		String siteId, readingValue, readingDate, readingId, readingType;
 		boolean ret = true;
+		int start, end;
+		LinkedList<String> studyNames = new LinkedList<String>();
 
 		try {
-			int i, studyPlaceholderIndex = -1;
+			// int i, studyPlaceholderIndex = -1;
 			Object obj = parser.parse(new FileReader(location));
 			JSONObject jObj = (JSONObject) obj;
 
-			JSONArray siteArray = (JSONArray) jObj.get("site_readings");
-			Iterator<String> siteScan = siteArray.iterator();
+			// Discover study names
+			String json = jObj.toString();
+			System.out.println(json);
 
-			// Add notApplicable first, so it can be the default.
-			studyPlaceholder.add(new Study("notApplicable", "-1"));
-
-			while (siteScan.hasNext()) {
-				i = 0;
-				Object readings = siteScan.next();
-				JSONObject jReadings = (JSONObject) readings;
-
-				// Read in site data.
-				siteId = String.valueOf(jReadings.get("site_id"));
-				readingValue = String.valueOf(jReadings.get("reading_value"));
-				readingDate = String.valueOf(jReadings.get("reading_date"));
-				readingId = String.valueOf(jReadings.get("reading_id"));
-				readingType = String.valueOf(jReadings.get("reading_type"));
-
-				// Sort out study information, e.g. where to put it & if study already exists
-				if (jReadings.containsKey("study_name") && jReadings.containsKey("study_id")) {
-					studyName = String.valueOf(jReadings.get("study_name"));
-					studyID = String.valueOf(jReadings.get("study_id"));
-
-					// Check if the study in this entry exists.
-					while (i < studyPlaceholder.size() && studyPlaceholderIndex == -1) {
-						if (studyPlaceholder.get(i).getNameOfStudy().compareToIgnoreCase(studyName) == 0
-								&& studyPlaceholder.get(i).getStudyID().compareToIgnoreCase(studyID) == 0)
-							studyPlaceholderIndex = i;
-						i++;
+			// Study names will always be before the pattern: ":{"
+			for (int i = 0; i < json.length() - 3; i++) {
+				if (json.substring(i, i + 4).compareTo("\":{\"") == 0) {
+					end = i;
+					start = end;
+					while (start > 0) {
+						start -= 1;
+						if (json.charAt(start) == '"') {
+							// get substring between start and end, and add it
+							// to studyNames, this is our study name
+							studyNames.add(json.substring(start + 1, end));
+							start = -1;
+						}
 					}
+				}
+			}
 
-					// Add the study if it doesn't exist and make sure studyPlaceholderIndex points
-					// to it
-					if (studyPlaceholderIndex == -1) {
-						// This will be the index after we add the new study
-						studyPlaceholderIndex = studyPlaceholder.size();
-						studyPlaceholder.add(new Study(studyName, studyID));
-					}
+			for (String n : studyNames)
+				System.out.println(n);
 
-					// After all that, if the collection is open, add the data.
-					if (studyPlaceholder.get(studyPlaceholderIndex).isCollectionOpen(siteId))
-						studyPlaceholder.get(studyPlaceholderIndex).getOrMakeSite(siteId).addReading(readingValue,
-								readingDate, readingId, readingType);
+			for (int i = 0; i < studyNames.size(); i++) {
+				// Get the study object
+				JSONObject studyArray = (JSONObject) jObj
+						.get(studyNames.get(i));
+				Study buf;
 
-				} else {
-					// Default is the notApplicable study. Add data if collection for site is open.
-					if (studyPlaceholder.get(0).isCollectionOpen(siteId))
-						studyPlaceholder.get(0).getOrMakeSite(siteId).addReading(readingValue, readingDate, readingId,
-								readingType);
+				// Get every site reading in the study object
+				JSONArray siteArray = (JSONArray) studyArray
+						.get("site_readings");
+				Iterator<String> siteScan = siteArray.iterator();
+
+				// Start prepping our output for this iteration
+				buf = new Study(studyNames.get(i), studyNames.get(i));
+
+				while (siteScan.hasNext()) {
+					Object readings = siteScan.next();
+					JSONObject jReadings = (JSONObject) readings;
+
+					// Read in site data.
+					siteId = String.valueOf(jReadings.get("site_id"));
+					readingValue = String
+							.valueOf(jReadings.get("reading_value"));
+					readingDate = String.valueOf(jReadings.get("reading_date"));
+					readingId = String.valueOf(jReadings.get("reading_id"));
+					readingType = String.valueOf(jReadings.get("reading_type"));
+
+					buf.getOrMakeSite(siteId).addReading(readingValue,
+							readingDate, readingId, readingType);
 				}
 
+				// Commit site data
+				studyPlaceholder.add(buf);
+			}
+
+			// Now see if there are any study-less sites.
+			JSONArray siteArray = (JSONArray) jObj.get("site_readings");
+			if (siteArray != null) {
+				// If there are, do the same thing as above, but with
+				// notApplicable.
+				studyPlaceholder
+						.add(new Study("notApplicable", "notApplicable"));
+				Iterator<String> siteScan = siteArray.iterator();
+				while (siteScan.hasNext()) {
+					Object readings = siteScan.next();
+					JSONObject jReadings = (JSONObject) readings;
+
+					siteId = String.valueOf(jReadings.get("site_id"));
+					readingValue = String
+							.valueOf(jReadings.get("reading_value"));
+					readingDate = String.valueOf(jReadings.get("reading_date"));
+					readingId = String.valueOf(jReadings.get("reading_id"));
+					readingType = String.valueOf(jReadings.get("reading_type"));
+
+					studyPlaceholder.get(studyPlaceholder.size() - 1)
+							.getOrMakeSite(siteId).addReading(readingValue,
+									readingDate, readingId, readingType);
+				}
 			}
 
 		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
+			ret = false;
 
+		} catch (IOException e) {
+			ret = false;
+
+		} catch (ParseException e) {
+			ret = false;
+		}
 		return ret;
 	}
-
 }
