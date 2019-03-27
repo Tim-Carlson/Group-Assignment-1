dd71159a58928f2b05a07a8be19cd5cd49d1436a
FromssaOlana <47232703+FromssaOlana@users.noreply.github.com>
2019-03-26 22:12:26 -0500

    Create JsonWriterTest.java
    I have tested it and it workes well.

---
+++ contents/TestFolder/JsonWriterTest.java
@@ -0,0 +1,42 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+import assignment_2.JsonWriter;
+import assignment_2.Site;
+import assignment_2.Study;
+
+class JsonWriterTest {
+	
+	// instantiating necessary objects. 
+	ArrayList<Study> studyList = new ArrayList<>();
+	Site site = new Site("001");
+	Study study1 = new Study("Alpha", "A001");
+	JsonWriter writer =  new JsonWriter();
+	
+	
+
+	@Test
+	void test() {
+		// populating a site object with readings. 
+		site.addReading("78.7C", "01/02/19", "001", "Temp");
+		site.addReading("35.8m/h", "01/02/19", "002", "Wind Speed");
+		site.addReading("38", "01/02/19", "003", "Humidity");
+		// adding a site object to a study object. 
+		study1.addSite(site);
+		studyList.add(study1);
+		// writing a file to a Json format. address 
+		
+		// (PLEASE CHANGE THE FILE ADDRESS FIRST TO YOUR LOCAL COMPUTER TO RUN THE REST.)
+		writer.write(" c:⁨Users⁩\\fromssaolana\\Desktop\\test001.json⁩", studyList);
+		
+		//checking if the file is written and exists. 
+		boolean result = new File(" c:⁨Users⁩\\fromssaolana\\Desktop\\test001.json⁩").isFile();
+		assertTrue(result);		
+		
+	}
+
+}
